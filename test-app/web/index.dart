2c2783d9edd162d22a42ae2f5766aff68e7eb3bd
jorinak <jorinak@princeton.edu>
2017-06-27 18:23:12 -0400

    Update index.dart

--- test-app/web/index.dart
+++ test-app/web/index.dart
@@ -1,13 +1,5 @@
 import 'dart:html';
-import 'package:sqljocky/sqljocky.dart';
 
 main() {
   querySelector('#text').text = 'Hello from Dart!';
-  
-  var pool = new ConnectionPool(host: 'us-cdbr-iron-east-03.cleardb.net',
-    port: 3306,
-    user: 'b18bc4480e14e6',
-    password: '71b480f7',
-    db: 'heroku_98e72a9715f019b',
-    max: 5);
 }
